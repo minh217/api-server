2f0177e22f010befe2c42e659ef4599365530e3e
MinhTang <minh.tang@mogi.vn>
2022-09-14 17:02:52 +0700

    stop put new

--- services/news.service.ts
+++ services/news.service.ts
@@ -1,6 +1,7 @@
 import { CRUD } from "../common/crud.inteface";
 import NewsDao from "../repositories/news/dao/news.dao";
-import { CreateNewDto } from "../repositories/news/dto/create.dto";
+import { CreateNewDto } from "../repositories/news/dto/create.new.dto";
+import { PutNewDto } from "../repositories/news/dto/put.new.dto";
 class NewsService implements CRUD{
     list = async (limit: number, page: number) => {
         return await NewsDao.getNews();
@@ -8,8 +9,12 @@ class NewsService implements CRUD{
     create = async (resource: CreateNewDto) => {
         return await NewsDao.createNew(resource);
     };
-    putById = async (resource: any) => Promise<any>;
-    readById = async (id: any) => Promise<any>;
+    putById = async (resource: PutNewDto) => {
+        return await NewsDao.putNew(resource);
+    };
+    readById = async (id: number) => {
+        return await NewsDao.getNewById(id);
+    };
     deleteById = async (id: any) => Promise<any>;
     patchById = async (id: any, resource: any) => Promise<any>;
     
